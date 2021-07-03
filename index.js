f2630e0a5c369dcbdb271d9de936d2e83e263e0e
yasar2385 <yasar.mohideen@newgen.co>
2021-07-04 01:23:50 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+var Arr = {};
+var objA = {
+  title: 'Alphabet',
+  hexValue: '00X0',
+  text: 'A'
+};
+var objB = {
+  title: 'Alphabet',
+  hexValue: '00X0',
+  text: 'A'
+};
+var objC = {
+  title: 'Alphabet',
+  hexValue: '00X0',
+  text: 'A'
+};
+var objD = {
+  title: 'Alphabet',
+  hexValue: '00X0',
+  text: 'A'
+};
+Arr[objA.text] = objA;
+Arr[objB.text] = objB;
+Arr[objC.text] = objC;
+Arr[objD.text] = objD;
+console.log(Arr);
