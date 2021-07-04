398fd23d0d0aa4f289b2143f07a5725b9f8ba3d0
yasar2385 <yasar.mohideen@newgen.co>
2021-07-04 13:15:03 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 import './style.css';
 import $ from 'jquery';
 
-
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
@@ -37,3 +36,17 @@ delete Arr['A'];
 console.log(Arr);
 Arr[objA.text] = objA;
 console.log(Arr);
+
+document.getElementById('pageedit').onchange = function() {
+  document.getElementById('pagelabel').textContent = this.value;
+};
+
+Array.from(document.querySelectorAll('.listEntry')).forEach(el => {
+  el.addEventListener('click', function() {
+    var val = this.getAttribute('data-PI-Value'),
+      id = this.id,
+      edit_id = id.split('_')[0] + '_edit',
+      label_id = id.split('_')[0] + '_label';
+    console.log(id, edit_id, label_id);
+  });
+});
